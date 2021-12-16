eb2aa0255e9ffec1c83db3b2067b95d071cfc7ec
Masato Yamashita <4653960+M-Yamashita01@users.noreply.github.com>
2021-12-16 22:52:15 +0900

    Add sample terraform files.

---
+++ membership.tf
@@ -0,0 +1,13 @@
+resource "github_membership" "membership_for_some_user" {
+  username = "SomeUser"
+  role     = "member"
+}
+resource "github_membership" "membership_for_some_user" {
+  username = "SomeUser1"
+  role     = "member"
+}
+
+resource "github_membership" "membership_for_some_user" {
+  username = "SomeUser2"
+  role     = "member"
+}
